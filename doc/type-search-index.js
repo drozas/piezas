eab5f3e9d22c2e3567ef17a4c2ab356d6511f2a6
David Rozas <david.rozas@gmail.com>
2023-09-28 14:18:40 +0200

    Quick generation of javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"tp1.ejemplos.piezas","l":"ListaPiezas"},{"p":"tp1.ejemplos.piezas","l":"Pieza"}];updateSearchResults();
