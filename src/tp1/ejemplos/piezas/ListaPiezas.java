4fb24b83ff98347e5049cf776f64a02fd1fde5d1
David Rozas <david.rozas@gmail.com>
2023-10-02 16:47:56 +0200

    Testing automatic indentation in Eclipse

--- src/tp1/ejemplos/piezas/ListaPiezas.java
+++ src/tp1/ejemplos/piezas/ListaPiezas.java
@@ -2,29 +2,32 @@ package tp1.ejemplos.piezas;
 
 /**
  * 
- * Implementa una lista de objetos de tipo Pieza con array estático y contador (Ejemplo para TP1 - UCM)
+ * Implementa una lista de objetos de tipo Pieza con array estático y contador
+ * (Ejemplo para TP1 - UCM)
  * 
  * @author drozas (basado en slides de Simon Pickin)
  *
  */
 public class ListaPiezas {
-    
-    // Número máximo de Piezas
-    private final static int MAX = 100;
-    // Array de Piezas
-    private Pieza[] piezas;
-    // Contador de piezas en array (coincidirá con la primera posición libre del array)
-    private int contador = 0;
-    
-    /**
-     * Crea un objeto de tipo lista de Piezas con un array de tamaño MAX
-     */
-    public ListaPiezas() {
-        this.piezas = new Pieza[MAX];
-    }
+
+	// Número máximo de Piezas
+	private final static int MAX = 100;
+	// Array de Piezas
+	private Pieza[] piezas;
+	// Contador de piezas en array (coincidirá con la primera posición libre del
+	// array)
+	private int contador = 0;
+
+	/**
+	 * Crea un objeto de tipo lista de Piezas con un array de tamaño MAX
+	 */
+	public ListaPiezas() {
+		this.piezas = new Pieza[MAX];
+	}
 
 	/**
 	 * Comprueba si la lista está llena
+	 * 
 	 * @return true si ya está llena, false en caso contrario
 	 */
 	public boolean llena() {
@@ -33,6 +36,7 @@ public class ListaPiezas {
 
 	/**
 	 * Comprueba si la lista está vacía
+	 * 
 	 * @return true si está vacía, false en caso contario
 	 */
 	public boolean vacia() {
@@ -41,21 +45,24 @@ public class ListaPiezas {
 
 	/**
 	 * Devuelve la longitud de la lista en el momento actual
+	 * 
 	 * @return número de elementos de la lista
 	 */
-	public int length() { 
+	public int length() {
 		return this.contador;
 	}
 
 	/**
-	 * Comprueba si una lista está llena y, en caso contrario, inserta una nueva pieza
-	 * @param p	Nueva pieza	
-	 * @return	true si se hizo la inserción correctamente, false en caso contrario
+	 * Comprueba si una lista está llena y, en caso contrario, inserta una nueva
+	 * pieza
+	 * 
+	 * @param p Nueva pieza
+	 * @return true si se hizo la inserción correctamente, false en caso contrario
 	 */
 	public boolean insertar(Pieza p) {
-		if (this.llena()){
+		if (this.llena()) {
 			return false;
-		}else{
+		} else {
 			this.piezas[this.contador] = p;
 			this.contador++;
 			return true;
@@ -64,17 +71,18 @@ public class ListaPiezas {
 
 	/**
 	 * Borra un elemento de la lista
+	 * 
 	 * @param pos posición del elemento a borrar
 	 * @return true si se borró, false en caso contraio
 	 */
-	public boolean borrar(int pos){
+	public boolean borrar(int pos) {
 		// Compruebo que la posición recibida esté dentro de los límites
-		if((pos < 0) || (pos > (this.contador - 1))){
+		if ((pos < 0) || (pos > (this.contador - 1))) {
 			return false;
-		}else{            
+		} else {
 			// Si es así, reasigno el resto de elementos desde la posición dada
-			for(int i=pos; i < (this.contador - 1); i++) {
-				this.piezas[i] = this.piezas[i+1];
+			for (int i = pos; i < (this.contador - 1); i++) {
+				this.piezas[i] = this.piezas[i + 1];
 			}
 			// Y decremento el contador
 			this.contador--;
@@ -84,13 +92,14 @@ public class ListaPiezas {
 
 	/**
 	 * Devuelve una pieza
+	 * 
 	 * @param pos posición en el array de la pieza a devolver
 	 * @return null si no la encuentra, o un objeto de tipo Pieza si la encuentra
 	 */
 	public Pieza get(int pos) {
-		if((pos < 0) || (pos > (this.contador - 1))){
+		if ((pos < 0) || (pos > (this.contador - 1))) {
 			return null;
-		}else{
+		} else {
 			return this.piezas[pos];
 		}
 
@@ -102,7 +111,7 @@ public class ListaPiezas {
 	public String toString() {
 		// %n en format: salto de línea independiente de plataforma
 		String cad = String.format("Elementos de la lista:%n%n");
-		for(int i = 0; i < this.contador; i++) {
+		for (int i = 0; i < this.contador; i++) {
 			// Se realizara una llamada implícita a toString() de Pieza
 			cad += String.format("%s ocupa la posición %d %n", this.piezas[i], i);
 		}
@@ -110,7 +119,7 @@ public class ListaPiezas {
 	}
 
 	public static void main(String[] args) {
-		//Creo tres piezas, mi lista, las añado y las imprimo
+		// Creo tres piezas, mi lista, las añado y las imprimo
 		Pieza p1 = new Pieza("Broca dim 6mm", 21);
 		Pieza p2 = new Pieza("Bateria", 165);
 		Pieza p3 = new Pieza("Correa ventilador", 63.98);
@@ -118,38 +127,38 @@ public class ListaPiezas {
 		miLista.insertar(p1);
 		miLista.insertar(p2);
 
-		//Puedo probar haciendo la lista más pequeña para ver si llena funciona
+		// Puedo probar haciendo la lista más pequeña para ver si llena funciona
 		if (miLista.insertar(p3) == false) {
 			System.out.println("En esta prueba puse MAX a 2 y no se pudo insertar la tercera pieza");
 		}
 
 		System.out.println(miLista);
 
-		//Recupero la pieza 0, le cambio el precio y la muestro
+		// Recupero la pieza 0, le cambio el precio y la muestro
 		Pieza p = miLista.get(0);
 		p.setCoste(4.1);
 		System.out.println("Pieza 0 ahora vale " + p.getCoste());
 
-		//Intento borrar la pieza en la posición 3 (devolverá false)
+		// Intento borrar la pieza en la posición 3 (devolverá false)
 		if (miLista.borrar(3)) {
 			System.out.println("Borré la pieza en 3");
-		}else {
+		} else {
 			System.out.println("No existía pieza en la posición 3 que intenté borrar");
 		}
 
-		//Intento borrar la pieza en la posición 1 (devolverá true)
+		// Intento borrar la pieza en la posición 1 (devolverá true)
 		if (miLista.borrar(0)) {
 			System.out.println("Borré la pieza en 0");
 		}
 
-		//Muestro de nuevo mi lista
+		// Muestro de nuevo mi lista
 		System.out.println(miLista);
 
-		//Añado una nueva pieza
+		// Añado una nueva pieza
 		Pieza nuevaPieza = new Pieza("Bombilla 10W", 9.80);
 		miLista.insertar(nuevaPieza);
 
-		//Muestro de nuevo mi lista
-		System.out.println(miLista); 	    
+		// Muestro de nuevo mi lista
+		System.out.println(miLista);
 	}
 }
--- src/tp1/ejemplos/piezas/Pieza.java
+++ src/tp1/ejemplos/piezas/Pieza.java
@@ -8,71 +8,76 @@ package tp1.ejemplos.piezas;
  *
  */
 public class Pieza {
-	
+
 	// Atributos de mi Pieza
 	private String nombre;
 	private double coste;
-	
+
 	/**
 	 * Construye una pieza
+	 * 
 	 * @param n nombre de la pieza
 	 * @param c coste de la pieza
 	 */
-	public Pieza (String n, double c) {
+	public Pieza(String n, double c) {
 		this.nombre = n;
 		this.coste = c;
 	}
-	
+
 	/**
-	 * Devuelve el nombre de la pieza	
+	 * Devuelve el nombre de la pieza
+	 * 
 	 * @return nombre de la pieza
 	 */
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	/**
 	 * Asigna un valor al nombre de la pieza
+	 * 
 	 * @param nombre nombre de la pieza
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	/**
 	 * Devuelve el coste de la pieza
+	 * 
 	 * @return coste de la pieza
 	 */
 	public double getCoste() {
 		return coste;
 	}
-	
+
 	/**
 	 * Asigna un coste a la pieza
+	 * 
 	 * @param coste coste de la pieza
 	 */
 	public void setCoste(double coste) {
 		this.coste = coste;
 	}
-	
+
 	/**
 	 * Devuelve una representación en formato cadena de la pieza
 	 */
 	public String toString() {
-		return "La pieza es "  + this.nombre + " y su coste es " + this.coste;
+		return "La pieza es " + this.nombre + " y su coste es " + this.coste;
 	}
-	
+
 	/**
 	 * Punto de entrada alternativo para testeo de objetos de tipo pieza
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Pieza p1 = new Pieza("Correa", 10);
 		Pieza p2 = new Pieza("Radiador", 20);
-		
+
 		System.out.println(p1);
 		System.out.println(p2);
 	}
-	
-	
+
 }
